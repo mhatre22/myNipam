b1c3eaa1ec3556a1f335a3c6d396a87ab3eeb6cc
mhatre22 <mhatresnehal75@gmail.com>
2024-04-27 23:52:51 +0530

    second commit

--- NimapProject/src/app/app.component.ts
+++ NimapProject/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NimapProject';
+  registerForm! : FormGroup ;
+  countries: String[] = ['India', 'Canada', 'USA', 'Australia', 'America', 'Kenia']
+  states: String[] = ['Maharashtra', 'Goa', 'Bihar', 'Manipur', 'Keral', 'Madhya Pradesh']
+  constructor( private fb:FormBuilder){
+  }
+  ngOnInit(){
+
+  
+  this.registerForm = this.fb.group({
+    //image: ['', [Validators.required]],
+    firstName: ['', [Validators.required]],
+    lastName:  ['', [Validators.required]], 
+  email: ['', [Validators.required, Validators.email]],
+   // age: ['', [Validators.required]], 
+    //tag:  ['', [Validators.required]], 
+    contact:  ['', [Validators.required]], 
+    country: ['', [Validators.required]], 
+    state:  ['', [Validators.required]], 
+    addressType:  ['home', [Validators.required]], 
+    address1:  ['', [Validators.required]], 
+    address2:  ['', [Validators.required]], 
+    companyAddress1:  ['', [Validators.required]], 
+    companyAddress2:  ['', [Validators.required]], 
+  });
+
+}
+Submit(){
+  console.log(this.registerForm.value);
+}
 }
